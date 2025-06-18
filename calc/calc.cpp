47c7497951734da6874a093115ef21275c8b22ab
wikim <unangel@yuhan.ac.kr>
2025-06-18 09:54:18 +0900

    1. calc/calc.cpp  [ * ] 함수 호출로 결과를 출력하도록 수정

--- calc/calc.cpp
+++ calc/calc.cpp
@@ -20,6 +20,6 @@ int main()
 	std::cin >> y;
 
 	printf("입력된 수 x[ %d ] y[ %d ]\n", x, y);
-	printf("add[ %d ] sub[ %d ] mul[ %d ] dive[ %d ] mod[ %d ]\n", 10, 6, 16, 4, 0);
-	//add(x, y), sub(x, y), mul(x, y), dive(x, y), mod(x, y));
+	printf("add[ %d ] sub[ %d ] mul[ %d ] dive[ %d ] mod[ %d ]\n",
+			add(x, y), sub(x, y), mul(x, y), dive(x, y), mod(x, y));
 }
