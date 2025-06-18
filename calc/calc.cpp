ac7e6505f232f8f82385dcbb33c035f5b39a4afe
wikim <unangel@yuhan.ac.kr>
2025-06-18 22:09:28 +0900

    1. calc.cpp  [ * ] 출력 내용 변경

--- calc/calc.cpp
+++ calc/calc.cpp
@@ -19,7 +19,7 @@ int main()
 	std::cout << "두번째 수를 입력하세요 : ";
 	std::cin >> y;
 
-	printf("입력된 수 x[ %d ] y[ %d ]\n", x, y);
+	printf("입력 x[ %d ] y[ %d ]\n", x, y);
 	printf("add[ %d ] sub[ %d ] mul[ %d ] dive[ %d ] mod[ %d ]\n", 10, 6, 16, 4, 0);
 	//add(x, y), sub(x, y), mul(x, y), dive(x, y), mod(x, y));
 }
